d3ce79293183534b974f3bac3ca36e706c1d442b
Kevin <kevinhnguyen9@gmail.com>
2023-04-21 15:20:32 +0100

    Added a unit test file

--- airports.py
+++ airports.py
@@ -13,10 +13,16 @@ console = Console(record=True)
 def get_search():
     return Prompt.ask("Search for an an airport")
 
-def loadAirportJSON():
+def load_airport_json():
     # Load airport data from airports.json
     f = open('airports.json')
 
     airportData = json.load(f)
     
     return airportData
+
+def find_matching_airports(name, airport_data):
+    possible_matches = [airport for airport in airport_data \
+                        if name.lower() in airport['name'].lower()]
+    
+    
