4261f96973402b17c94ff07c035d91e34952c10a
Kevin <kevinhnguyen9@gmail.com>
2023-04-25 14:05:30 +0100

    Started type hinting and started verify user choice function

--- airports.py
+++ airports.py
@@ -10,19 +10,39 @@ from rich.theme import Theme
 
 console = Console(record=True)
 
-def get_search():
+def get_search() -> str:
+    """Prompts the user to enter an airport to search
+
+    Returns:
+        str: user's chosen airport
+    """
     return Prompt.ask("Search for an an airport")
 
-def load_airport_json():
-    # Load airport data from airports.json
+def load_airport_json() -> dict:
+    """Opens airport.json file and loads it, to access airport data
+
+    Returns:
+        dict: Dictionary containing airports and information
+    """
     f = open('airports.json')
 
     airportData = json.load(f)
     
     return airportData
 
-def verify_user_airport_choice(possible_matches):
-    
+def verify_user_airport_choice(possible_matches:list):
+    airport_choices = [airports['name'] for airports in possible_matches]
+
+    selection = None
+
+    while selection == None:
+        user_choice = Prompt.ask("Please select one out of: " + str(airport_choices))
+        
+        for airports in airport_choices:
+            if user_choice.lower() == airports['name'].lower():
+                selection = airports
+                return selection
+            
 
 def find_matching_airports(name, airport_data):
     possible_matches = [airport for airport in airport_data \
